2fa00332c74d526443f431ceff7288baca448f93
js5966 <93935138+js5966@users.noreply.github.com>
2022-06-05 17:50:53 -0400

    Update index.html and .js

--- docs/county-map.js
+++ docs/county-map.js
@@ -1,8 +1,7 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQpk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQ';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/jsarwate/cl41qtq4r002g14n2wnrn7ke3',
+    style: 'mapbox://styles/jsarwate/cl41tbewp000c14o6f8hsgpd9',
     zoom: 6.5,
     center: [-85.5, 37.7]
 });
-
--- docs/police-map.js
+++ docs/police-map.js
@@ -1,8 +1,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQ';
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/jsarwate/cl3uhu68k001f14n9t35kdhva',
+    style: 'mapbox://styles/jsarwate/cl41tbewp000c14o6f8hsgpd9',
     zoom: 6.5,
     center: [-85.5, 37.7]
 });
-
