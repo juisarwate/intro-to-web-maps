f4e0706a79567f7c36954ebc17f4ba631c776ca1
js5966 <93935138+js5966@users.noreply.github.com>
2022-06-05 17:33:42 -0400

    Update index.html

---
+++ docs/county-map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQpk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/jsarwate/cl41qtq4r002g14n2wnrn7ke3',
+    zoom: 6.5,
+    center: [-85.5, 37.7]
+});
+
---
+++ docs/police-map.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianNhcndhdGUiLCJhIjoiY2t4NnI5ZjJyMDRucjJwcnl5NDh1Zml5cSJ9.qmI6QbbKDZ98r06dRnkSzQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/jsarwate/cl3uhu68k001f14n9t35kdhva',
+    zoom: 6.5,
+    center: [-85.5, 37.7]
+});
+
